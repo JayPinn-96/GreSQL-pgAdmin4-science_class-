9a097ad5b60f64f81c3139d66ac17860724ec5d5
JayPinn-96 <130509745+JayPinn-96@users.noreply.github.com>
2023-05-08 19:14:03 +0100

    Update SQL data analysis.sql

--- SQL data analysis.sql
+++ SQL data analysis.sql
@@ -5,6 +5,12 @@ SELECT * FROM dbo.['2019$']
 UNION
 SELECT * FROM dbo.['2020$'])
 
+SELECT
+arrival_date_year,
+hotel,
+round(sum((stays_in_week_nights+stays_in_weekend_nights)*adr),2) as revenue
+from hotels
+group by arrival_date_year, hotel
 
 
 SELECT * FROM hotels
